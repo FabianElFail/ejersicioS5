2551a537e30b0d51cf2cd775a7a592fc46d5a4fb
FabianElFail <fabianecasasolivos@hotmail.com>
2024-09-12 12:16:32 -0500

    Ahora si todo xd

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -3,17 +3,18 @@
     private static void Main(string[] args)
     {
         int i = 1;
-        String contra = "";
-        
+        String contra = "",contra2;
+        Console.WriteLine("Registre su contraseña");
+        contra2 = Console.ReadLine();
         while (i<=3) {
             Console.WriteLine("Ingrese su contraseña");
             contra = Console.ReadLine();
-            if (contra == "casas")
+            if (contra2 == contra)
             {
                 Console.WriteLine("CONTRASEÑA CORRECTA");
                 Console.WriteLine("BIENVENIDO");
                 i = 4;
-            } else if (contra != "casas") {
+            } else if (contra2 != contra) {
                 Console.WriteLine("Intento fallido");
                 if (i == 3)
                 {
